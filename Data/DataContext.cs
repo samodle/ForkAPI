b47fa531365c54c55aafd9dc2dce32f9ff137e22
Sam Odle <sro27@cornell.edu>
2020-08-01 12:57:37 -0400

    + sqlite db

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using ForkApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForkApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> MyProperty { get; set; }
+    }
+}
