bdca1ec410cbce13cffe24be90d2b85e74a343a1
Sam Odle <sro27@cornell.edu>
2020-08-02 20:51:41 -0400

    updated database, added security features

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -24,7 +24,7 @@ namespace DatingApp.API.Controllers
         {
             var values = await _context.Values.ToListAsync();
 
-            return Ok(values);
+            return Ok(values); 
         }
 
         // GET api/values/5
---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using ForkApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForkApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+
+        Task<bool> UserExists(string username);
+    }
+}
